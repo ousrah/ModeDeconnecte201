055b27f25fead85efcf562795c3729b9affd50a0
Oussama Rahmouni <ousrah@hotmail.com>
2022-01-28 18:12:54 +0100

    Gestion des medecins

--- ModeDeconnecte201/FrmMedecin.Designer.cs
+++ ModeDeconnecte201/FrmMedecin.Designer.cs
@@ -30,34 +30,128 @@ namespace ModeDeconnecte201
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lstMedecin = new System.Windows.Forms.ListBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.txtTelephone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnSuivant = new System.Windows.Forms.Button();
+            this.txtRecherche = new System.Windows.Forms.TextBox();
+            this.btnRecherche = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::ModeDeconnecte201.Properties.Resources.doctor;
-            this.pictureBox1.Location = new System.Drawing.Point(564, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(1320, -3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(224, 215);
+            this.pictureBox1.Size = new System.Drawing.Size(336, 336);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // lstMedecin
+            // 
+            this.lstMedecin.FormattingEnabled = true;
+            this.lstMedecin.ItemHeight = 25;
+            this.lstMedecin.Location = new System.Drawing.Point(58, 79);
+            this.lstMedecin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstMedecin.Name = "lstMedecin";
+            this.lstMedecin.Size = new System.Drawing.Size(309, 354);
+            this.lstMedecin.TabIndex = 2;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(446, 129);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(249, 30);
+            this.txtNom.TabIndex = 4;
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(446, 166);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(249, 30);
+            this.txtPrenom.TabIndex = 5;
+            // 
+            // txtTelephone
+            // 
+            this.txtTelephone.Location = new System.Drawing.Point(446, 203);
+            this.txtTelephone.Name = "txtTelephone";
+            this.txtTelephone.Size = new System.Drawing.Size(249, 30);
+            this.txtTelephone.TabIndex = 6;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(446, 250);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(249, 30);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // btnSuivant
+            // 
+            this.btnSuivant.Location = new System.Drawing.Point(303, 455);
+            this.btnSuivant.Name = "btnSuivant";
+            this.btnSuivant.Size = new System.Drawing.Size(75, 44);
+            this.btnSuivant.TabIndex = 8;
+            this.btnSuivant.Text = ">";
+            this.btnSuivant.UseVisualStyleBackColor = true;
+            this.btnSuivant.Click += new System.EventHandler(this.btnSuivant_Click);
+            // 
+            // txtRecherche
+            // 
+            this.txtRecherche.Location = new System.Drawing.Point(58, 29);
+            this.txtRecherche.Name = "txtRecherche";
+            this.txtRecherche.Size = new System.Drawing.Size(249, 30);
+            this.txtRecherche.TabIndex = 9;
+            // 
+            // btnRecherche
+            // 
+            this.btnRecherche.Location = new System.Drawing.Point(313, 29);
+            this.btnRecherche.Name = "btnRecherche";
+            this.btnRecherche.Size = new System.Drawing.Size(116, 30);
+            this.btnRecherche.TabIndex = 10;
+            this.btnRecherche.Text = "Chercher";
+            this.btnRecherche.UseVisualStyleBackColor = true;
+            this.btnRecherche.Click += new System.EventHandler(this.btnRecherche_Click);
+            // 
             // FrmMedecin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1659, 1055);
+            this.Controls.Add(this.btnRecherche);
+            this.Controls.Add(this.txtRecherche);
+            this.Controls.Add(this.btnSuivant);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTelephone);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.lstMedecin);
             this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmMedecin";
             this.Text = "FrmMedecin";
+            this.Load += new System.EventHandler(this.FrmMedecin_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox lstMedecin;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.TextBox txtTelephone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnSuivant;
+        private System.Windows.Forms.TextBox txtRecherche;
+        private System.Windows.Forms.Button btnRecherche;
     }
 }
--- ModeDeconnecte201/FrmMedecin.cs
+++ ModeDeconnecte201/FrmMedecin.cs
@@ -6,14 +6,64 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ModeDeconnecte201
 {
     public partial class FrmMedecin : Form
     {
+
+        private string cs = @"data source=.\sqlexpress2008;initial catalog=cabinetMedecin;user id=sa;Password=P@ssw0rd";
+        SqlConnection cn = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        DataSet ds = new DataSet();
+        SqlDataAdapter da = new SqlDataAdapter();
+        BindingSource bs = new BindingSource();
+
         public FrmMedecin()
         {
             InitializeComponent();
+        }
+
+        private void FrmMedecin_Load(object sender, EventArgs e)
+        {
+            cn.ConnectionString = cs;
+            cn.Open();
+            com.Connection = cn;
+            com.CommandText = "select * from medecin";
+
+            da.SelectCommand = com;
+            da.Fill(ds, "Medecin");
+
+            bs.DataSource = ds;
+            bs.DataMember = "Medecin";
+
+            lstMedecin.DisplayMember = "nom";
+            lstMedecin.ValueMember = "id";
+            lstMedecin.DataSource = bs;
+
+        /*    comboBox1.DisplayMember = "nom";
+            comboBox1.ValueMember = "id";
+            comboBox1.DataSource = bs;*/
+
+         //   dataGridView1.DataSource = bs;
+
+            txtNom.DataBindings.Add("Text", bs, "nom");
+            txtPrenom.DataBindings.Add("Text", bs, "prenom");
+            txtTelephone.DataBindings.Add("Text", bs, "telephone");
+            txtEmail.DataBindings.Add("Text", bs, "email");
+
+
+        }
+
+        private void btnSuivant_Click(object sender, EventArgs e)
+        {
+            bs.MoveNext();
+        }
+
+        private void btnRecherche_Click(object sender, EventArgs e)
+        {
+            bs.Filter = "nom like '%" + txtRecherche.Text + "%'";
         }
     }
 }
