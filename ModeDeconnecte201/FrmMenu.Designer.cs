62dceda955fbd63a80fee7480fb710177c19e193
Oussama Rahmouni <ousrah@hotmail.com>
2022-01-28 17:37:58 +0100

    Ajoutez des fichiers projet.

---
+++ ModeDeconnecte201/FrmMenu.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace ModeDeconnecte201
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPatient = new System.Windows.Forms.ToolStripButton();
+            this.btnConsultation = new System.Windows.Forms.ToolStripButton();
+            this.btnRendezVous = new System.Windows.Forms.ToolStripButton();
+            this.btnFermer = new System.Windows.Forms.ToolStripButton();
+            this.btnMedecin = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(100, 100);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnMedecin,
+            this.btnPatient,
+            this.btnConsultation,
+            this.btnRendezVous,
+            this.btnFermer});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1296, 135);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 135);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1296, 656);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnPatient
+            // 
+            this.btnPatient.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPatient.Image = global::ModeDeconnecte201.Properties.Resources.examination;
+            this.btnPatient.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPatient.Name = "btnPatient";
+            this.btnPatient.Size = new System.Drawing.Size(104, 132);
+            this.btnPatient.Text = "Patient";
+            this.btnPatient.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnPatient.Click += new System.EventHandler(this.btnPatient_Click);
+            // 
+            // btnConsultation
+            // 
+            this.btnConsultation.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultation.Image = global::ModeDeconnecte201.Properties.Resources.medical_record;
+            this.btnConsultation.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnConsultation.Name = "btnConsultation";
+            this.btnConsultation.Size = new System.Drawing.Size(136, 132);
+            this.btnConsultation.Text = "Consultation";
+            this.btnConsultation.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            // 
+            // btnRendezVous
+            // 
+            this.btnRendezVous.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRendezVous.Image = global::ModeDeconnecte201.Properties.Resources.timetable;
+            this.btnRendezVous.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnRendezVous.Name = "btnRendezVous";
+            this.btnRendezVous.Size = new System.Drawing.Size(138, 132);
+            this.btnRendezVous.Text = "Rendez-Vous";
+            this.btnRendezVous.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            // 
+            // btnFermer
+            // 
+            this.btnFermer.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFermer.Image = global::ModeDeconnecte201.Properties.Resources.close;
+            this.btnFermer.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFermer.Name = "btnFermer";
+            this.btnFermer.Size = new System.Drawing.Size(104, 132);
+            this.btnFermer.Text = "Fermer";
+            this.btnFermer.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnFermer.Click += new System.EventHandler(this.btnFermer_Click);
+            // 
+            // btnMedecin
+            // 
+            this.btnMedecin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMedecin.Image = global::ModeDeconnecte201.Properties.Resources.doctor;
+            this.btnMedecin.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnMedecin.Name = "btnMedecin";
+            this.btnMedecin.Size = new System.Drawing.Size(104, 132);
+            this.btnMedecin.Text = "Medecin";
+            this.btnMedecin.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnMedecin.Click += new System.EventHandler(this.btnMedecin_Click);
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1296, 791);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FrmMenu";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnPatient;
+        private System.Windows.Forms.ToolStripButton btnConsultation;
+        private System.Windows.Forms.ToolStripButton btnRendezVous;
+        private System.Windows.Forms.ToolStripButton btnFermer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripButton btnMedecin;
+    }
+}
+
---
+++ ModeDeconnecte201/FrmMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ModeDeconnecte201
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMedecin_Click(object sender, EventArgs e)
+        {
+            FrmMedecin f = new FrmMedecin();
+            AfficherFenetre(f);
+        }
+
+        private void btnFermer_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AfficherFenetre(Form f)
+        {
+           
+            panel1.Controls.Clear();
+           f.TopLevel = false;
+            f.AutoScroll = true;
+            f.FormBorderStyle = FormBorderStyle.None;
+            this.panel1.Controls.Add(f);
+            f.Show();
+        }
+
+        private void btnPatient_Click(object sender, EventArgs e)
+        {
+            FrmPatient f = new FrmPatient();
+            AfficherFenetre(f);
+        }
+    }
+}
